5562f679071f59643df5cd4c2f9b00d61b46df4e
David <davidllumitaxi67@gmail.com>
2023-11-07 00:08:39 -0500

    Agregar archivos de proyecto.

---
+++ CompuMod_v2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace CompuMod_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ptbPixel = new System.Windows.Forms.PictureBox();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnDegradado = new System.Windows.Forms.Button();
+            this.btnLetra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPixel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(251, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COMPUTACIÓN CIENTÍFICA";
+            // 
+            // ptbPixel
+            // 
+            this.ptbPixel.BackColor = System.Drawing.Color.White;
+            this.ptbPixel.Location = new System.Drawing.Point(51, 45);
+            this.ptbPixel.Name = "ptbPixel";
+            this.ptbPixel.Size = new System.Drawing.Size(600, 500);
+            this.ptbPixel.TabIndex = 1;
+            this.ptbPixel.TabStop = false;
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Location = new System.Drawing.Point(1, 561);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(75, 23);
+            this.btnEncender.TabIndex = 2;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnDegradado
+            // 
+            this.btnDegradado.Location = new System.Drawing.Point(163, 561);
+            this.btnDegradado.Name = "btnDegradado";
+            this.btnDegradado.Size = new System.Drawing.Size(75, 23);
+            this.btnDegradado.TabIndex = 3;
+            this.btnDegradado.Text = "Degradado";
+            this.btnDegradado.UseVisualStyleBackColor = true;
+            this.btnDegradado.Click += new System.EventHandler(this.btnDegradado_Click);
+            // 
+            // btnLetra
+            // 
+            this.btnLetra.Location = new System.Drawing.Point(82, 561);
+            this.btnLetra.Name = "btnLetra";
+            this.btnLetra.Size = new System.Drawing.Size(75, 23);
+            this.btnLetra.TabIndex = 4;
+            this.btnLetra.Text = "Letra";
+            this.btnLetra.UseVisualStyleBackColor = true;
+            this.btnLetra.Click += new System.EventHandler(this.btnLetra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(718, 655);
+            this.Controls.Add(this.btnLetra);
+            this.Controls.Add(this.btnDegradado);
+            this.Controls.Add(this.btnEncender);
+            this.Controls.Add(this.ptbPixel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPixel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ptbPixel;
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnDegradado;
+        private System.Windows.Forms.Button btnLetra;
+    }
+}
+
---
+++ CompuMod_v2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompuMod_v2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pixelVec = new Bitmap(600, 500); //Demencion de la ventana
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDegradado_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 600; i++)
+            {
+                for(int j = 0; j < 500; j++)
+                {
+                    pixelVec.SetPixel(i, j, Color.FromArgb((int)(-0.118 * i + 178), (int)(-0.071 * i + 170), (int)(0.32 * i + 49)));
+                }
+            }
+            ptbPixel.Image = pixelVec;
+        }
+
+        private void btnLetra_Click(object sender, EventArgs e)
+        {
+            // Coordenadas del dibujo
+            int x = 270, y = 250;
+            
+            for(int i = 270; i <= 320; i++)
+            {
+                pixelVec.SetPixel(i, y, Color.Blue);
+            }
+
+            for (int j = 180; j <= 250; j++)
+            {
+                pixelVec.SetPixel(x, j, Color.Blue);
+            }
+            ptbPixel.Image = pixelVec;
+        }
+    }
+}
